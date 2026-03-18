a39e9159bc508950ed0bb156a4fc342f9d5d4496
Philip <oumaphilip01@gmail.com>
2026-03-18 20:22:19 +0300

    feat: add firebase configurations, android and ios settings for biometrics permission, connect app to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 import 'views/biometric/biometric_auth_wrapper.dart';
 import 'services/auth_service.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => AuthService(),
